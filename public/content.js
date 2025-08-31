2f2dd15f0199f949d2b263a7ff07e876920515f9
Luffy <yedaxia@foxmail.com>
2025-08-31 11:04:22 +0800

    修复截图重复bug

--- public/content.js
+++ public/content.js
@@ -1,19 +1,18 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Message received in content script:', request);
-  if (request.action === 'capture') {
-    const video = document.querySelector('video');
-    console.log('Found video element:', video);
-    if (video) {
-      const canvas = document.createElement('canvas');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      const dataUrl = canvas.toDataURL('image/png');
-      console.log('Sending message to runtime with dataUrl:', dataUrl);
-      chrome.runtime.sendMessage({ action: 'new_frame', dataUrl: dataUrl });
-    } else {
-      console.log('No video element found');
+if (typeof window.contentScriptInjected === 'undefined') {
+  window.contentScriptInjected = true;
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'capture') {
+      const video = document.querySelector('video');
+      if (video) {
+        const canvas = document.createElement('canvas');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const dataUrl = canvas.toDataURL('image/png');
+        chrome.runtime.sendMessage({ action: 'new_frame', dataUrl: dataUrl });
+      }
     }
-  }
-});
+  });
+}
